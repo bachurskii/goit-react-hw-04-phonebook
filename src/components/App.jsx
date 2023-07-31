600ffe9181500919b47904b74a6bbd5d4e3fff09
Georgen1 <123319264+bachurskii@users.noreply.github.com>
2023-07-31 11:14:51 +0300

    updated my repositorii

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { nanoid } from 'nanoid';
 import ContactList from './contacts/contacts';
 import Filter from './filter/filter';
 import Form from './form/form';
 
-const LocalSrtorageContacts = 'contacts';
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contacts: [],
-      filter: '',
-    };
-  }
-  componentDidMount() {
-    const savedContacts = localStorage.getItem(LocalSrtorageContacts);
+const LocalStorageContactsKey = 'contacts';
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const prevContactsRef = useRef();
+
+  useEffect(() => {
+    const savedContacts = localStorage.getItem(LocalStorageContactsKey);
     if (savedContacts) {
-      this.setState({ contacts: JSON.parse(savedContacts) });
+      setContacts(JSON.parse(savedContacts));
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevState, prevProps) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem(
-        LocalSrtorageContacts,
-        JSON.stringify(this.state.contacts)
-      );
+  useEffect(() => {
+    if (prevContactsRef.current !== contacts) {
+      localStorage.setItem(LocalStorageContactsKey, JSON.stringify(contacts));
     }
-  }
+    prevContactsRef.current = contacts;
+  }, [contacts]);
 
-  handleAddContact = (name, number) => {
-    const { contacts } = this.state;
+  const handleAddContact = (name, number) => {
     if (
       contacts.some(
         contact => contact.name.toLowerCase() === name.toLowerCase()
@@ -41,49 +37,44 @@ class App extends Component {
     }
 
     const newContact = { id: nanoid(), name, number };
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+    setContacts(prevContacts => [...prevContacts, newContact]);
   };
 
-  handleFilterChange = event => {
-    this.setState({ filter: event.target.value });
+  const handleFilterChange = event => {
+    setFilter(event.target.value);
   };
 
-  handleDeleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  };
-
-  render() {
-    const { contacts, filter } = this.state;
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
+  const handleDeleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
     );
+  };
 
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          fontSize: 20,
-          color: '#010101',
-        }}
-      >
-        <h1>Phonebook</h1>
-        <Form onAddContact={this.handleAddContact} />
-        <h2>Contacts</h2>
-        <Filter value={filter} onChange={this.handleFilterChange} />
-        <ContactList
-          contacts={filteredContacts}
-          onDeleteContact={this.handleDeleteContact}
-        />
-      </div>
-    );
-  }
-}
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontSize: 20,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <Form onAddContact={handleAddContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={handleDeleteContact}
+      />
+    </div>
+  );
+};
 
 export default App;
